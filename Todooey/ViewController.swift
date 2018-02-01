753d5b57448187662d66ee03cee7615a328cbe4c
Andrew Hogue <hogue@Andrews-MacBook-Pro.local>
2018-02-01 08:55:32 -0500

    Initial Commit

---
+++ Todooey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Todooey
+//
+//  Created by Andrew Hogue on 2018-02-01.
+//  Copyright © 2018 Andrew Hogue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
